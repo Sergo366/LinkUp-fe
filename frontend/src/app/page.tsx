7dd1343fdcb66b3af76eade3572f4b64725aac5e
Serhii Kozenkov <kozenkovs366@gmail.com>
2024-02-29 22:19:09 +0200

    Add links

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link';
+
 export const metadata = {
 	title: "about codeevolution"
 }
 
 const Home = () => {
 	return (
-		<h1>HomePage</h1>
+		<>
+			<h1>HomePage</h1>
+			<Link href={"/blog"}>Blog</Link>
+			<Link href={"/about"}>About</Link>
+		</>
 	);
 };
 
