e9335731ccea7923792164b313153ee13fe325a9
Serhii Kozenkov <kozenkovs366@gmail.com>
2024-02-29 21:49:55 +0200

    Add dynamic metadata

--- frontend/src/app/blog/[blogId]/page.tsx
+++ frontend/src/app/blog/[blogId]/page.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import { Metadata } from 'next';
 
-const First = ({params}: {params: {blogId: string}}) => {
+
+type Props = {
+	params: {
+		blogId: string
+	}
+}
+
+export const generateMetadata = async ({params}:Props): Promise<Metadata> => {
+	const title = await new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Iphone 13 ")
+		}, 300)
+	})
+
+	return {
+		title: `Blog ${title}`
+	}
+}
+
+const First = ({params}: Props) => {
 	return (
 		<div>
 			First {params.blogId}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+	title: "about codeevolution"
+}
+
 const Home = () => {
 	return (
 		<h1>HomePage</h1>
