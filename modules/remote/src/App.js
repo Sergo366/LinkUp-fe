06ce07cefef02ec63196cca0c3ffc25a9f2409ec
skozenkov <skozenkov@deloitte.com>
2024-05-25 15:43:53 +0300

    Basic micro-frontend logic

---
+++ modules/remote/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "./Button";
+
+export const App = () => {
+  return <div>
+    Hello from the other side 66
+    <Button/>
+  </div>;
+};
+export default App;
